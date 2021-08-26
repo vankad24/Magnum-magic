3af0b519bd5d29880ab1858e8fabfe197f7be591
Vaiker <vankad24@mail.ru>
2021-08-26 15:10:18 +0800

    Add magic creative tab

--- src/main/java/com/magnum/magic/MainMagic.java
+++ src/main/java/com/magnum/magic/MainMagic.java
@@ -4,7 +4,9 @@ import com.magnum.magic.blocks.LampBlock;
 import com.magnum.magic.blocks.MagicOreBlock;
 import com.magnum.magic.blocks.MagicStoneBlock;
 import com.magnum.magic.items.MagicDustItem;
+import com.magnum.magic.items.MyItem;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -22,17 +24,24 @@ public class MainMagic {
     public static final String NAME = "";
     public static final String VERSION = "1.0";
 
+    MyItem dust;
+    MagicItemGroup magicItemGroup = new MagicItemGroup("magicGroup");
+
+
+
     public MainMagic() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
 
-        Register.goToRegistration(new LampBlock(), ItemGroup.BUILDING_BLOCKS);
-        Register.goToRegistration(new MagicStoneBlock(), ItemGroup.BUILDING_BLOCKS);
-        Register.goToRegistration(new MagicOreBlock(),ItemGroup.BUILDING_BLOCKS);
+        //Blocks
+        Register.goToRegistration(new LampBlock(), magicItemGroup);
+        Register.goToRegistration(new MagicStoneBlock(), magicItemGroup);
+        Register.goToRegistration(new MagicOreBlock(),magicItemGroup);
 
 
-        Register.goToRegistration(new MagicDustItem(ItemGroup.MISC));
-        //   /give @p magmagic:lamp
+        //Items
+        Register.goToRegistration(dust = new MagicDustItem(magicItemGroup));
+
 
     }
 
@@ -50,6 +59,18 @@ public class MainMagic {
      */
     private void clientSetup(final FMLClientSetupEvent event){
 
+    }
+
+    public class MagicItemGroup extends ItemGroup{
+
+        public MagicItemGroup(String name) {
+            super(name);
+        }
+
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(dust);
+        }
     }
 
 }
